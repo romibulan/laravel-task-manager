3f784704c9ed353e48a5b387bda329eafd88fdba
sumantra dey <sumantradey@learningspiral.co.in>
2026-05-12 09:43:40 +0530

    http changed

--- frontend/src/utils/http.js
+++ frontend/src/utils/http.js
@@ -14,6 +14,7 @@ axios.interceptors.request.use(config => {
     console.log('Setting baseURL for Sanctum CSRF request');
     const newBase = ""; //import.meta.env.VITE_SANCTUM_URL;
     // Ensure the URL is not already absolute before prepending
+    axios.defaults.baseURL = newBase;
     if (!config.url.startsWith('http')) {
       config.url = `${newBase}${config.url}`;
     }
