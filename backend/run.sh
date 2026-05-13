6b0f5e701a1f0fb4a6446f6ce97b33058d2211c0
sumantra dey <sumantradey@learningspiral.co.in>
2026-05-13 23:33:56 +0530

    removed supervisor cmd

--- backend/run.sh
+++ backend/run.sh
@@ -12,6 +12,3 @@ echo "Running migrations..."
 # --force is required in production
 php artisan migrate --force
 
-echo "Starting Nginx and PHP-FPM..."
-# This command depends on your base image's supervisor/process manager
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
