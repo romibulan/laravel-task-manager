0c663b12c1647b72263b73445c3d51b4e0a027de
sumantra dey <sumantradey@learningspiral.co.in>
2026-05-15 08:46:10 +0530

    csrf desabled for logout

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(
             except: [
                 '/api/login',
+                '/api/logout',
                 '/api/register',
                 '/api/tasks', '/api/tasks/*',
             ]
